aa214cfd5c0cac72ac85cfb06aa9b886c56273a0
Ke Zhai <kzhai@umd.edu>
2011-04-12 22:46:04 -0400

    add in wikipedia parser

--- src/topmod/io/io_adapter.py
+++ src/topmod/io/io_adapter.py
@@ -41,11 +41,11 @@ def output_param(alpha, beta, gamma, dir, index=-1):
         for k in gamma[doc].keys():
             f.write(str(doc) + "\t" + str(k) + "\t" + str(gamma[doc][k]) + "\n")
             
-def output(d):
-    f = open("/windows/d/Workspace/data/test_data", "w");
+def output(data, file):
+    f = open(file, "w");
     
     terms = [];
-    for value in d.values():
+    for value in data.values():
         terms = terms+value;
     terms = set(terms);
     
@@ -56,13 +56,38 @@ def output(d):
         i+=1;
     
     i = 1;
-    for doc in d.keys():
-        if len(d[doc])==0:
+    for doc in data.keys():
+        if len(data[doc])==0:
             continue;
         f.write(str(i) + "\t");
-        temp = set(d[doc]);
+        temp = set(data[doc]);
         for t in temp:
-            f.write(str(termID[t])+ "\t"+str(d[doc].count(t))+"\t");
+            f.write(str(termID[t])+ "\t"+str(data[doc].count(t))+"\t");
+        f.write("\n");
+        i+=1;
+        
+def append(data, file):
+    f = open(file, "a");
+    
+    terms = [];
+    for value in data.values():
+        terms = terms+value;
+    terms = set(terms);
+    
+    termID = {}
+    i = 0;
+    for t in terms:
+        termID[t] = i;
+        i+=1;
+    
+    i = 1;
+    for doc in data.keys():
+        if len(data[doc])==0:
+            continue;
+        f.write(str(i) + "\t");
+        temp = set(data[doc]);
+        for t in temp:
+            f.write(str(termID[t])+ "\t"+str(data[doc].count(t))+"\t");
         f.write("\n");
         i+=1;
         
