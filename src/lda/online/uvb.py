33ae1042149efcd7f23c78ea9289f54cc1183ddf
Zhai, Ke <kzhai@umd.edu>
2011-11-09 16:58:50 -0500

    check in

--- src/lda/online/uvb.py
+++ src/lda/online/uvb.py
@@ -15,21 +15,21 @@ References:
 [1] M. Hoffman, D. Blei, and F. Bach. Online Learning for Latent Dirichlet Allocation. Neural Information Processing Systems, 2010
 """
 class UncollapsedVariationalBayes(VariationalBayes):
-    def __init__(self, alpha_update_decay_factor=0.9, 
-             alpha_maximum_decay=10, 
-             gamma_converge_threshold=0.000001, 
-             gamma_maximum_iteration=100, 
-             alpha_converge_threshold = 0.000001, 
-             alpha_maximum_iteration = 100, 
-             model_likelihood_threshold = 0.00001,
-             global_maximum_iteration = 100,
-             snapshot_interval = 10):
-        super(UncollapsedVariationalBayes, self).__init__(alpha_update_decay_factor, 
-                                                          alpha_maximum_decay, 
-                                                          gamma_converge_threshold, 
-                                                          gamma_maximum_iteration, 
-                                                          alpha_converge_threshold, 
-                                                          alpha_maximum_iteration, 
+    def __init__(self, alpha_update_decay_factor=0.9,
+             alpha_maximum_decay=10,
+             gamma_converge_threshold=0.000001,
+             gamma_maximum_iteration=100,
+             alpha_converge_threshold=0.000001,
+             alpha_maximum_iteration=100,
+             model_likelihood_threshold=0.00001,
+             global_maximum_iteration=100,
+             snapshot_interval=10):
+        super(UncollapsedVariationalBayes, self).__init__(alpha_update_decay_factor,
+                                                          alpha_maximum_decay,
+                                                          gamma_converge_threshold,
+                                                          gamma_maximum_iteration,
+                                                          alpha_converge_threshold,
+                                                          alpha_maximum_iteration,
                                                           global_maximum_iteration,
                                                           snapshot_interval);
     
@@ -42,15 +42,15 @@ class UncollapsedVariationalBayes(VariationalBayes):
         super(UncollapsedVariationalBayes, self)._initialize(data, num_topics);
         
         # initialize a D-by-K matrix gamma, valued at N_d/K
-        self._gamma = numpy.tile(self._alpha + 1.0*self._V/self._K, (self._D, 1));
+        self._gamma = numpy.tile(self._alpha + 1.0 * self._V / self._K, (self._D, 1));
         self._gamma = numpy.exp((scipy.special.psi(self._gamma) - scipy.special.psi(numpy.sum(self._gamma, 1))[:, numpy.newaxis]));
         
         # initialize a V-by-K matrix beta, valued at 1/V, subject to the sum over every row is 1
-        self._lambda = 1*numpy.random.gamma(100., 1./100., (self._V, self._K));
+        self._lambda = 1 * numpy.random.gamma(100., 1. / 100., (self._V, self._K));
         self._log_beta = scipy.special.psi(self._lambda) - scipy.special.psi(numpy.sum(self._lambda, axis=0)[numpy.newaxis, :]);
         
-        self._eta = 1.0/self._K;
-        self._batch_size=batch_size;
+        self._eta = 1.0 / self._K;
+        self._batch_size = batch_size;
         
         self._tau = tau;
         self._kappa = kappa;
@@ -65,33 +65,33 @@ class UncollapsedVariationalBayes(VariationalBayes):
     @deprecated: no longer useful
     """
     def update_phi(self, doc_id, phi_table):
-        self._gamma[[doc_id], :] = numpy.random.gamma(100., 1./100., (1, self._K))
+        self._gamma[[doc_id], :] = numpy.random.gamma(100., 1. / 100., (1, self._K))
         self._gamma[[doc_id], :] = numpy.exp(scipy.special.psi(self._gamma[[doc_id], :]) - scipy.special.psi(numpy.sum(self._gamma[[doc_id], :])));
         
         # update phi and gamma until gamma converges
         for gamma_iteration in xrange(self._gamma_maximum_iteration):
             term_ids = numpy.array(self._data[doc_id].keys());
             term_counts = numpy.array([self._data[doc_id].values()]);
-            assert(term_counts.shape==(1, len(term_ids)));
+            assert(term_counts.shape == (1, len(term_ids)));
             
             phi_contribution = self._log_beta[term_ids, :] + numpy.log(self._gamma[[doc_id], :]);
             #phi_contribution = self._log_beta[term_ids, :] + scipy.special.psi(self._gamma[[doc_id], :]);
-            phi_normalizer = numpy.log(numpy.sum(numpy.exp(phi_contribution), axis=1)[:, numpy.newaxis]+1e-100);
-            assert(phi_normalizer.shape==(len(term_ids), 1));
+            phi_normalizer = numpy.log(numpy.sum(numpy.exp(phi_contribution), axis=1)[:, numpy.newaxis] + 1e-100);
+            assert(phi_normalizer.shape == (len(term_ids), 1));
             phi_contribution -= phi_normalizer;
             
-            assert(phi_contribution.shape==(len(term_ids), self._K));
+            assert(phi_contribution.shape == (len(term_ids), self._K));
             phi_contribution += numpy.log(term_counts.transpose());
             
             gamma_update = self._alpha + numpy.array(numpy.sum(numpy.exp(phi_contribution), axis=0));
             gamma_update = numpy.exp(scipy.special.psi(gamma_update) - scipy.special.psi(numpy.sum(gamma_update)));
             mean_change = numpy.mean(abs(gamma_update - self._gamma[doc_id, :]));
             self._gamma[[doc_id], :] = gamma_update;
-            if mean_change<=self._gamma_converge_threshold:
+            if mean_change <= self._gamma_converge_threshold:
                 break;
             
         likelihood_phi = numpy.sum(numpy.exp(phi_contribution) * ((self._log_beta[term_ids, :] * term_counts.transpose()) - phi_contribution));
-        assert(phi_contribution.shape==(len(term_ids), self._K));
+        assert(phi_contribution.shape == (len(term_ids), self._K));
         #phi_table /= self._batch_size;
         #phi_table += self._eta;
         #phi_table = numpy.ones((self._V, self._K))*self._eta*self._batch_size;
@@ -102,7 +102,7 @@ class UncollapsedVariationalBayes(VariationalBayes):
     """
     """
     def learning(self, iteration=0):
-        if iteration<=0:
+        if iteration <= 0:
             iteration = self._global_maximum_iteration;
         
         for i in xrange(iteration):
@@ -120,35 +120,39 @@ class UncollapsedVariationalBayes(VariationalBayes):
                 # retrieve the term ids and their associated counts
                 term_ids = numpy.array(self._data[doc_id].keys());
                 term_counts = numpy.array([self._data[doc_id].values()]);
-                assert(term_counts.shape==(1, len(term_ids)));
+                assert(term_counts.shape == (1, len(term_ids)));
                 
                 # update phi and gamma until gamma converges
                 for gamma_iteration in xrange(self._gamma_maximum_iteration):
-                    # compute the phi contribution                                        
+                    # compute the phi contribution                    
+                    # @attention: note self._gamma in this case is exp(E[log(theta)]), this is different from vanilla variational inference for lda.
                     phi_contribution = self._log_beta[term_ids, :] + numpy.log(self._gamma[[doc_id], :]);
-                    phi_normalizer = numpy.log(numpy.sum(numpy.exp(phi_contribution), axis=1)[:, numpy.newaxis]+1e-100);
-                    assert(phi_normalizer.shape==(len(term_ids), 1));
+                    phi_normalizer = numpy.log(numpy.sum(numpy.exp(phi_contribution), axis=1)[:, numpy.newaxis] + 1e-100);
+                    assert(phi_normalizer.shape == (len(term_ids), 1));
                     phi_contribution -= phi_normalizer;
-                    assert(phi_contribution.shape==(len(term_ids), self._K));
+                    assert(phi_contribution.shape == (len(term_ids), self._K));
                     phi_contribution += numpy.log(term_counts.transpose());
                     
                     # compute the updated gamma
                     gamma_update = self._alpha + numpy.array(numpy.sum(numpy.exp(phi_contribution), axis=0));
+                    # @attention: note we are computing the exp(E[log(theta)]) and store it to self._gamma.
                     gamma_update = numpy.exp(scipy.special.psi(gamma_update) - scipy.special.psi(numpy.sum(gamma_update)));
                     mean_change = numpy.mean(abs(gamma_update - self._gamma[doc_id, :]));
                     self._gamma[[doc_id], :] = gamma_update;
-                    if mean_change<=self._gamma_converge_threshold:
+                    if mean_change <= self._gamma_converge_threshold:
                         break;
                     
                 #likelihood_phi += numpy.sum(numpy.exp(phi_contribution) * ((self._log_beta[term_ids, :] * term_counts.transpose()) - phi_contribution));
-                assert(phi_contribution.shape==(len(term_ids), self._K));
-                phi_table[[term_ids], :] += numpy.exp(phi_contribution)/self._batch_size;
+                assert(phi_contribution.shape == (len(term_ids), self._K));
+                phi_table[[term_ids], :] += numpy.exp(phi_contribution) / self._batch_size;
                 #alpha_sufficient_statistics += (scipy.special.psi(self._gamma[[doc_id], :]) - scipy.special.psi(numpy.sum(self._gamma[[doc_id], :], axis=1)));
                             
             rho = numpy.power((self._tau + i), -self._kappa);
-            self._lambda = (1.0-rho) * self._lambda + rho * phi_table;
+            # @attention: note we are updating lambda, which is a variational dirichet prior for beta matrix. unlike self._gamma, we need to explicitly store self._lambda and use it for update.
+            self._lambda = (1.0 - rho) * self._lambda + rho * phi_table;
+            # @attention: note we are computing exp(E[log(theta)]) and store it in log scale in this case.
             self._log_beta = (scipy.special.psi(self._lambda) - scipy.special.psi(numpy.sum(self._lambda, 0))[numpy.newaxis, :]);
-            assert(self._log_beta.shape==(self._V, self._K));
+            assert(self._log_beta.shape == (self._V, self._K));
 
             # compute the log-likelihood of alpha terms
             #alpha_sum = numpy.sum(self._alpha, axis=1);
@@ -161,7 +165,7 @@ class UncollapsedVariationalBayes(VariationalBayes):
     
             #new_likelihood = likelihood_alpha + likelihood_gamma + likelihood_phi;
             #print "em iteration is ", (i+1), " likelihood is ", new_likelihood
-            print "em iteration is", (i+1), "rho is", rho
+            print "em iteration is", (i + 1), "rho is", rho
             
             alpha_sufficient_statistics = scipy.special.psi(self._gamma) - scipy.special.psi(numpy.sum(self._gamma, axis=1)[:, numpy.newaxis]);
             alpha_sufficient_statistics = numpy.sum(alpha_sufficient_statistics, axis=0)[numpy.newaxis, :];
@@ -174,11 +178,11 @@ if __name__ == "__main__":
     temp_directory = "../../../data/de-news/en/corpus-2/";
     #temp_directory = "../../../data/test/";
     from util.input_parser import import_monolingual_data;
-    d = import_monolingual_data(temp_directory+"doc.dat");
+    d = import_monolingual_data(temp_directory + "doc.dat");
     
     lda = UncollapsedVariationalBayes();
     lda._initialize(d, 20, 0.01, 0.7);
     lda.learning(50);
     print lda._log_beta
     print lda._gamma
-    lda.print_topics(temp_directory+"voc.dat");
+    lda.print_topics(temp_directory + "voc.dat");
